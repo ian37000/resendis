13cf380de37d5e064d4cb0d0271ac741974f7e51
ian37000 <160811483+ian37000@users.noreply.github.com>
2024-06-24 22:18:51 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/resendis/Resendis.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.resendis;
+
+/**
+ *
+ * @author PC GAMER
+ */
+public class Resendis extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+       MenuP menu =new MenuP();
+       menu.setVisible(true);
+       menu.setLocationRelativeTo(null);
+    }
+}
